0ebb3078e912ce389cfc3db500e1fa61ae7d9d7b
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-26 01:39:51 +0900

    あとから可変にするのは可能

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let mut u = User {
+    let u = User {
         name: String::from("john"),
         age: 32,
     };
 
-    u.age += 1;
-    println!("name: {}, age: {}", u.name, u.age);
+    let mut u_mutable = u;
+    u_mutable.age += 1;
+    println!("name: {}, age: {}", u_mutable.name, u_mutable.age);
 }
 
 struct User {
