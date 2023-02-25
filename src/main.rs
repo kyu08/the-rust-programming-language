66a4551471318d8689ea3e797e1c239bbe063619
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-26 01:54:46 +0900

    構造体の初期化時、変数名とフィールド名が同じであればkey:value形式ではなくvalueだけ書けばよい

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 fn main() {
-    let u = User {
-        name: String::from("john"),
-        age: 32,
-    };
-
-    let mut u_mutable = u;
-    u_mutable.age += 1;
-    println!("name: {}, age: {}", u_mutable.name, u_mutable.age);
+    let u = build_user(String::from("john"), 32);
+    println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
 }
 
 struct User {
     name: String,
     age: u32,
+    active: bool,
+}
+
+fn build_user(name: String, age: u32) -> User {
+    User {
+        name,
+        age,
+        active: true,
+    }
 }
