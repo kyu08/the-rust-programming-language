0eaf39a83cca2b5801ff4e167569aa40ac1fecd4
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-26 01:57:10 +0900

    ...u みたいに書くと構造体の一部のフィールドを更新できる

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     let u = build_user(String::from("john"), 32);
     println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
+
+    let u = add_age(u);
+    println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
 }
 
 struct User {
@@ -14,5 +17,12 @@ fn build_user(name: String, age: u32) -> User {
         name,
         age,
         active: true,
+    }
+}
+
+fn add_age(u: User) -> User {
+    User {
+        age: u.age + 1,
+        ..u
     }
 }
