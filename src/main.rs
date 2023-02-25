ca9b168b9ffc3055249e49e543f0efd52d24ea31
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-26 02:25:14 +0900

    関連関数, メソッド

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let u = build_user(String::from("john"), 32);
-    // println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
+    let mut u = User::build_user(String::from("john"), 32);
+    println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
 
-    let u = add_age(u);
-    // println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
-    println!("{:?}", u);
-    println!("{:#?}", u);
+    println!("is_active_adult: {}", u.is_active_adult());
+
+    u.add_age();
+    println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
 }
 
 #[derive(Debug)]
@@ -15,17 +15,19 @@ struct User {
     active: bool,
 }
 
-fn build_user(name: String, age: u32) -> User {
-    User {
-        name,
-        age,
-        active: true,
+impl User {
+    fn build_user(name: String, age: u32) -> User {
+        User {
+            name,
+            age,
+            active: true,
+        }
     }
-}
+    fn is_active_adult(&self) -> bool {
+        self.active && 20 <= self.age
+    }
 
-fn add_age(u: User) -> User {
-    User {
-        age: u.age + 1,
-        ..u
+    fn add_age(&mut self) {
+        self.age += 1;
     }
 }
