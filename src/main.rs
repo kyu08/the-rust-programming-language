c4e3a04e270a47da6d871cb7944a28df43b7ab92
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-27 02:13:05 +0900

    vectorの要素にfor in でアクセス

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
     let v = vec![1, 2, 3, 4, 5];
-    let third: &i32 = &v[2];
-    println!("{}", third);
 
-    match v.get(20) {
-        Some(v) => println!("{}", v),
-        None => println!("none"),
+    for item in v {
+        println!("{}", item);
     }
 }
