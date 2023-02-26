d11883f620f4d9e1ed9a90e08d5ea641faeca5f5
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-26 13:46:13 +0900

    match

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,27 @@
-fn main() {
-    let mut u = User::build_user(String::from("john"), 32);
-    println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
-
-    println!("is_active_adult: {}", u.is_active_adult());
-
-    u.add_age();
-    println!("name: {}, age: {}, active: {}", u.name, u.age, u.active);
-}
-
 #[derive(Debug)]
-struct User {
-    name: String,
-    age: u32,
-    active: bool,
+enum UsState {
+    Alaska,
+    NewYork,
 }
 
-impl User {
-    fn build_user(name: String, age: u32) -> User {
-        User {
-            name,
-            age,
-            active: true,
-        }
-    }
-    fn is_active_adult(&self) -> bool {
-        self.active && 20 <= self.age
-    }
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
 
-    fn add_age(&mut self) {
-        self.age += 1;
-    }
+fn main() {
+    let coin = Coin::Penny;
+    let value = match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("state:{:?}!", state);
+            25
+        }
+    };
+
+    println!("{}", value);
 }
