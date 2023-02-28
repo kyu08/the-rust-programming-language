ff01474b64288647fb37fa7cd4a3006398ea740c
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-01 01:18:56 +0900

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
+struct NewsFeed {}
+impl Summary for NewsFeed {
+    fn summarize(&self) -> String {
+        String::from("news!")
+    }
+}
+
+struct Tweet {}
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        String::from("tweet!")
+    }
+}
+
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
 fn main() {
-    let v = vec![1, 2, 3];
-    v[10];
+    let n = NewsFeed {};
+    let t = Tweet {};
+
+    say(n);
+    say(t);
+}
+
+fn say(summary: impl Summary) {
+    println!("{}", summary.summarize());
 }
